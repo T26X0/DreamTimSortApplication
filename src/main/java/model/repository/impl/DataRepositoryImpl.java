73f76933fb6ac687b30c07e201070ac43103ad0e
Danila <w4neex@gmail.com>
2024-10-24 00:10:14 +0300

    реализована функциональность чтения данных из текстового файла

--- src/main/java/model/repository/impl/DataRepositoryImpl.java
+++ src/main/java/model/repository/impl/DataRepositoryImpl.java
@@ -1,11 +1,26 @@
 package model.repository.impl;
 
 import model.repository.DataRepository;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class DataRepositoryImpl implements DataRepository {
 
     @Override
     public String getData() {
-        return null;
+        StringBuilder data = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(getClass().getResourceAsStream("/file.txt"), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line.trim()).append("\n"); // Чтение строки с файла и удаление лишних пробелов
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Логирование исключения
+        }
+        return data.toString(); //Возвращение итоговой строки
     }
 }
