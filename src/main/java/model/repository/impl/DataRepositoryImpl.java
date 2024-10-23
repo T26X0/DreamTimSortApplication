bf3782b33e66bc568255cb6749e98f812d1eeb93
t26x0 <panovd379@gmail.com>
2024-10-24 02:04:59 +0500

    refactor: Исправлено наследование

--- src/main/java/model/repository/impl/DataRepositoryImpl.java
+++ src/main/java/model/repository/impl/DataRepositoryImpl.java
@@ -1,11 +1,12 @@
 package model.repository.impl;
 
 import model.repository.DataRepository;
+import model.repository.exception.NoSuchFileException;
 
 public class DataRepositoryImpl implements DataRepository {
 
     @Override
-    public String getData() {
+    public String getData() throws NoSuchFileException {
         return null;
     }
 }
