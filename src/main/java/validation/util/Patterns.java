9eb73a8ad8d27204eea6c9d4d88afb5d9df4d364
sssewqx <ruthlessgod192@gmail.com>
2024-10-24 18:03:10 +0400

    Rename source string pattern const

--- src/main/java/validation/util/Patterns.java
+++ src/main/java/validation/util/Patterns.java
@@ -2,7 +2,7 @@ package validation.util;
 
 public class Patterns {
 
-    public static final String ORIGIN_STRING_PATTERN = "(?i)^([a-zA-Z][a-zA-Z0-9]*\\((?:[a-zA-Z]+|[0-9]+),(?:[a-zA-Z]+|[0-9]+),(?:[a-zA-Z]+|[0-9]+)\\);?)+$";
+    public static final String SOURCE_STRING_PATTERN = "(?i)^([a-zA-Z][a-zA-Z0-9]*\\((?:[a-zA-Z]+|[0-9]+),(?:[a-zA-Z]+|[0-9]+),(?:[a-zA-Z]+|[0-9]+)\\);?)+$";
     public static final String ANIMAL_PATTERN = "(?i)^animal\\\\(([a-zA-Z]+),\\\\s*([a-zA-Z]+),\\\\s*([a-zA-Z]+)\\\\)$";
     public static final String BARREL_PATTERN = "(?i)^barrel\\\\((\\\\d{1,3}),\\\\s*([a-zA-Z]+),\\\\s*([a-zA-Z]+)\\\\)$";
     public static final String HUMAN_PATTERN = "(?i)^human\\\\(([a-zA-Z]+),\\\\s*(\\\\d{1,3}),\\\\s*([a-zA-Z])\\\\)$";
