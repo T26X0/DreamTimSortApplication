65af3dc0d107930790637a187e83f05baedf7417
Danila <w4neex@gmail.com>
2024-10-24 15:05:47 +0300

    feat: добавлена обработка отсутствия файла

--- src/main/java/model/repository/impl/DataRepositoryImpl.java
+++ src/main/java/model/repository/impl/DataRepositoryImpl.java
@@ -2,28 +2,35 @@ package model.repository.impl;
 
 import model.repository.DataRepository;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import model.repository.exception.NoSuchFileException;
 
 public class DataRepositoryImpl implements DataRepository {
 
-    @Override
+    private static final String FILE_PATH = "src/main/resources/file.txt";
 
+    @Override
     public String getData() throws NoSuchFileException {
         StringBuilder data = new StringBuilder();
 
-        InputStream fileStream = getClass().getResourceAsStream("/file.txt");
+        File file = new File(FILE_PATH);
 
-        if (fileStream == null) { // Проверка, найден ли файл
-            throw new NoSuchFileException("Файл /file.txt не найден в ресурсах.");
+        if (!file.exists()) {
+            try {
+                Files.createFile(Paths.get(FILE_PATH));
+                System.out.println("Искомый файл не был найден. Директория обновлена и создан новый файл для записи данных. Можете заполнять и вернуться сюда снова после заполнения файла.");
+            } catch (Exception e) {
+                e.printStackTrace(); // Логирование исключения
+            }
+            return ""; // Возвращаем пустую строку, так как файл не существовал
         }
 
         try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(fileStream, StandardCharsets.UTF_8))) {
+                new InputStreamReader(new FileInputStream(FILE_PATH), StandardCharsets.UTF_8))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 data.append(line.trim()).append("\n"); // Чтение строки с файла и удаление лишних пробелов
