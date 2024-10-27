e521634a978ba02925639b4e271941e2c76b1139
t26x0 <panovd379@gmail.com>
2024-10-27 23:16:49 +0500

    feat: доабвил бинарный выÐпоиск

--- src/main/java/controller/data/search/binary/BinarySearch.java
+++ src/main/java/controller/data/search/binary/BinarySearch.java
@@ -1,8 +1,10 @@
 package controller.data.search.binary;
 
 import controller.data.search.binary.exception.EmptyCacheException;
+import model.entity.sortable.Sortable;
 import model.repository.constants.EntityPatternsRegex;
 
+import java.util.Comparator;
 import java.util.List;
 
 public interface BinarySearch<T> {
@@ -10,4 +12,6 @@ public interface BinarySearch<T> {
     T findEntity(EntityPatternsRegex entityPattern) throws EmptyCacheException;
 
     List<T> findByField(String field);
+
+    int binarySearchBySpecies(List<T> allSortableEntities, T targetSortableEntity, Comparator<? super T> unsortedSubList);
 }
--- src/main/java/controller/data/search/binary/impl/BinarySearchImpl.java
+++ src/main/java/controller/data/search/binary/impl/BinarySearchImpl.java
@@ -1,16 +1,21 @@
 package controller.data.search.binary.impl;
 
 import controller.data.DataController;
+import controller.data.comparator.GeneralComparatorUtil;
 import controller.data.impl.DataControllerImpl;
 import controller.data.search.binary.BinarySearch;
 import controller.data.search.binary.exception.EmptyCacheException;
+import model.entity.Animal;
 import model.entity.sortable.Sortable;
 import model.repository.constants.EntityPatternsRegex;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
-public class BinarySearchImpl implements BinarySearch<Sortable> {
+public class BinarySearchImpl<T>
+//        implements BinarySearch<Sortable>
+{
 
     private final DataController dataController;
 
@@ -19,7 +24,7 @@ public class BinarySearchImpl implements BinarySearch<Sortable> {
     }
 
     // TODO: Task on 27.10
-    @Override
+//    @Override
     public Sortable findEntity(EntityPatternsRegex entityPattern) throws EmptyCacheException {
         Sortable result = null;
         if (dataController.cacheIsClear()) throw new EmptyCacheException("Кэш приложения пуст.");
@@ -34,9 +39,67 @@ public class BinarySearchImpl implements BinarySearch<Sortable> {
         return result;
     }
 
-    @Override
+//    @Override
     public List<Sortable> findByField(String field) {
 
         return null;
+    }
+
+//    @Override
+    public int binarySearchBySpecies(List<T> allSortableEntities, T targetSortableEntity, Comparator<? super T> comparator) {
+        int left = 0;
+        int right = allSortableEntities.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            T midEntity = allSortableEntities.get(mid);
+
+            int comparison = comparator.compare(midEntity, targetSortableEntity);
+            if (comparison == 0) {
+                return mid; // Найдено
+            } else if (comparison < 0) {
+                left = mid + 1; // Ищем в правой половине
+            } else {
+                right = mid - 1; // Ищем в левой половине
+            }
+        }
+
+        return -1; // Не найдено
+    }
+}
+
+class Test {
+
+    public static void main(String[] args) {
+        BinarySearchImpl<Sortable> binarySearch = new BinarySearchImpl<>();
+
+        int i = binarySearch.binarySearchBySpecies(List.of(
+                new Animal.Builder()
+                        .species("a")
+                        .eyeColor("a")
+                        .hair(true)
+                        .build(),
+                new Animal.Builder()
+                        .species("a")
+                        .eyeColor("b")
+                        .hair(true)
+                        .build(),
+                new Animal.Builder()
+                        .species("b")
+                        .eyeColor("a")
+                        .hair(false)
+                        .build(),
+                new Animal.Builder()
+                        .species("b")
+                        .eyeColor("b")
+                        .hair(false)
+                        .build()
+        ), new Animal.Builder()
+                .species("b")
+                .eyeColor("a")
+                .hair(false)
+                .build(),
+                GeneralComparatorUtil.getComparatorForSortableEntity());
+        System.out.println(i);
     }
 }
