fe8a6d9060abddbd49d57434536d8191a954263b
t26x0 <panovd379@gmail.com>
2024-10-28 13:25:31 +0500

    feat: Добавлена возможность бинарного поиска id по заданной сущности в общем кэше приложения

--- src/main/java/controller/data/search/binary/BinarySearch.java
+++ src/main/java/controller/data/search/binary/BinarySearch.java
@@ -2,15 +2,12 @@ package controller.data.search.binary;
 
 import controller.data.search.binary.exception.EmptyCacheException;
 import model.constants.EntityPatternsRegex;
+import model.entity.sortable.Sortable;
 
 import java.util.Comparator;
 import java.util.List;
 
 public interface BinarySearch<T> {
 
-    T findEntity(EntityPatternsRegex entityPattern) throws EmptyCacheException;
-
-    List<T> findByField(String field);
-
-    int binarySearchBySpecies(List<T> allSortableEntities, T targetSortableEntity, Comparator<? super T> unsortedSubList);
+    int binarySearchBySpecies(List<T> allSortableEntities, T targetSortableEntity, Comparator<T> comparator);
 }
--- src/main/java/controller/data/search/binary/impl/BinarySearchImpl.java
+++ src/main/java/controller/data/search/binary/impl/BinarySearchImpl.java
@@ -3,6 +3,7 @@ package controller.data.search.binary.impl;
 import controller.data.DataController;
 import controller.data.comparator.GeneralComparatorUtil;
 import controller.data.impl.DataControllerImpl;
+import controller.data.search.binary.BinarySearch;
 import controller.data.search.binary.exception.EmptyCacheException;
 import model.entity.Animal;
 import model.entity.sortable.Sortable;
@@ -12,7 +13,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class BinarySearchImpl<T>
-//        implements BinarySearch<Sortable>
+        implements BinarySearch<T>
 {
 
     private final DataController dataController;
@@ -21,30 +22,10 @@ public class BinarySearchImpl<T>
         this.dataController = new DataControllerImpl();
     }
 
-    // TODO: Task on 27.10
-//    @Override
-    public Sortable findEntity(EntityPatternsRegex entityPattern) throws EmptyCacheException {
-        Sortable result = null;
-        if (dataController.cacheIsClear()) throw new EmptyCacheException("Кэш приложения пуст.");
-        switch (entityPattern) {
-            case BARREL_PATTERN -> {
-            }
-            case ANIMAL_PATTERN -> {
-            }
-            case HUMAN_PATTERN -> {
-            }
-        }
-        return result;
-    }
-
-//    @Override
-    public List<Sortable> findByField(String field) {
-
-        return null;
-    }
-
-//    @Override
-    public int binarySearchBySpecies(List<T> allSortableEntities, T targetSortableEntity, Comparator<? super T> comparator) {
+    @Override
+    public int binarySearchBySpecies(List<T> allSortableEntities,
+                                     T targetSortableEntity,
+                                     Comparator<T> comparator) {
         int left = 0;
         int right = allSortableEntities.size() - 1;
 
@@ -64,12 +45,33 @@ public class BinarySearchImpl<T>
 
         return -1; // Не найдено
     }
+
+//    @Override
+    public Sortable findEntity(EntityPatternsRegex entityPattern) throws EmptyCacheException {
+        Sortable result = null;
+        if (dataController.cacheIsClear()) throw new EmptyCacheException("Кэш приложения пуст.");
+        switch (entityPattern) {
+            case BARREL_PATTERN -> {
+            }
+            case ANIMAL_PATTERN -> {
+            }
+            case HUMAN_PATTERN -> {
+            }
+        }
+        return result;
+    }
+//    @Override
+
+    public List<Sortable> findByField(String field) {
+
+        return null;
+    }
 }
 
 class Test {
 
     public static void main(String[] args) {
-        BinarySearchImpl<Sortable> binarySearch = new BinarySearchImpl<>();
+        BinarySearchImpl binarySearch = new BinarySearchImpl();
 
         int i = binarySearch.binarySearchBySpecies(List.of(
                 new Animal.Builder()
